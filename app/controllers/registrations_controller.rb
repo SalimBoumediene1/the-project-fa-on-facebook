8a652b6d4aa1a4d6ae35bb186166fbc0d3727088
Salim Boumediene <salimboumediene@gmail.com>
2018-11-01 12:36:58 +0100

    kenadi

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,15 +5,39 @@ class RegistrationsController < ApplicationController
     end
 
     def new
-        puts "new"
         @user = User.new
     end
 
     def create
-        puts "create"
         @user = User.new
-
+        @user.name = params[:name]
+        @user.email = params[:email]
+        @user.password = params[:password]
+        @user.save
         redirect_to('/')
+    end
+
+    def login
+    
+    end
+
+    def connexion
+        puts params
+        @user_email = User.find_by(email: params[:email])
+        @user_password = User.find_by(password: params[:password])
+        if @user_email != params[:email] && @user_password != params[:password]
+            redirect_to login_path, alert: "Watch it, mister!"
+        else
+            flash[:notice] = "Connexion reussi"
+            redirect_to registrations_path
+        end
 
     end
+        def new_gossip
+            @gossip = Gossip.new
+        end
+
+        def create_gossip
+
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: 'registrations#index'
+  get '/login', to: 'registrations#login'
+  post '/login', to: 'registrations#connexion'
+  get '/gossip/:gossip', to: 'registrations#gossip', as: 'gossip'
   resources :registrations
 end
